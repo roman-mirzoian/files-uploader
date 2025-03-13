e330ed98df44b9d1723d674a0031d85bebab71e1
Roman <mirzo1930@gmail.com>
2025-03-14 00:42:40 +0100

    feat: delete unusable tests

--- server/src/files/files.service.ts
+++ server/src/files/files.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FileEntity } from './files.entity';
 import { DataSource, Repository } from 'typeorm';
-import { GoogleService } from 'src/google/google.service';
+import { GoogleService } from '../google/google.service';
 import { drive_v3 } from 'googleapis';
 
 @Injectable()
